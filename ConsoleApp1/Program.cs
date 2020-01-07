57b84e4653a72e94369d78aaf11c9afbeaddd493
Paulo Roberto <paulo030303paulo@gmail.com>
2020-01-07 08:53:56 -0300

    Projeto Iniciado

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo rt = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            rt.Largura = Convert.ToDouble(Console.ReadLine());
+            rt.Altura = Convert.ToDouble(Console.ReadLine());
+
+           Console.WriteLine("Area : " + rt.Area() );
+           Console.WriteLine("O Perimetro e: " + rt.Perimetro());
+            Console.WriteLine("A Diagonal mede: " + rt.Diagonal());
+        }
+    }
+}
---
+++ ConsoleApp1/Retangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+          double area = Largura * Altura;
+            return area;
+        }
+
+        public double Perimetro()
+        {
+            double peri = (Largura + Altura) * 2;
+            return peri;
+        }
+
+        public double Diagonal()
+        {
+            double diag = Math.Sqrt(Largura * Largura + Altura * Altura);
+            return diag;
+                 
+        }
+    }
+}
